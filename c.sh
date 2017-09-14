eab35d19008b5d3a60fc5936281eff7ba9400d06
wenlizhe <lizhe.wen@juphoon.com.cn>
2017-09-14 17:24:27 +0800

    914

--- c.sh
+++ c.sh
@@ -1,37 +1,46 @@
 #!/bin/bash
 
-success_num=0   #成功次数
-fail_num=0      #失败次数
-n=10            #执行次数
-ClientPerRoom=2 #每个会议加入人数
-MeetingNum=4    #测试服务器编号
+success_num=0   # 成功次数
+fail_num=0      # 失败次数
+n=10            # 执行次数
+ClientPerRoom=2 # 每个会议加入人数
+MeetingNum=4    # 测试服务器编号
 
 # 判断会议是否创建成功
 function read_file_create(){
     for line in `cat $1`
     do
-#        if [ `echo ${line} | grep -E "10[0-9]{6}"` ];then
-        if [ `echo ${line} | grep -E "[0-9]{5}"` ];then
+        if [ `echo ${line} | grep -E "10[0-9]{6}"` ];then
+#        if [ `echo ${line} | grep -E "[0-9]{5}"` ];then
             echo "meeting id" ${line} >>./text/create.txt
-#            echo "meeting id" ${line}
-#            ./join.sh ClientPerRoom ${line} 4 |tee ./text/join.txt |sleep 2 |exit
-            return 1
+            echo "meeting id" ${line}
+            # todo 脚本加入会议成员会自动退一个，不知什么原因，暂时每次多加一个成员
+            ./join.sh ${ClientPerRoom} ${line} ${MeetingNum} |tee ./text/join.txt |sleep 2 |exit
+            for line in `cat ./text/join.txt`
+            do
+                if [ `echo ${line} | grep -E "MtcConfJoinOkNotification"` ];then
+                    echo  ${line} >>./text/success.txt
+                    return 1
+                fi
+            done
+            return 0
+#            ./join.sh ${ClientPerRoom} ${line} ${MeetingNum}
         fi
     done
     return 0
 }
 
 # 判断加入会议是否成功
-function read_file_join(){
-    for line in `cat $1`
-        do
-            if [ `echo ${line} | grep -E "MtcConfJoinOkNotification"` ];then
-                echo  ${line} >>./text/success.txt
-                return 1
-            fi
-        done
-    return 0
-}
+#function read_file_join(){
+#    for line in `cat $1`
+#        do
+#            if [ `echo ${line} | grep -E "MtcConfJoinOkNotification"` ];then
+#                echo  ${line} >>./text/success.txt
+#                return 1
+#            fi
+#        done
+#    return 0
+#}
 
 #function count_result(){
 #    i=0
@@ -65,7 +74,7 @@ function read_file_join(){
 # 判断流程是否成功
 function do_work() {
     read_file_create $1
-    read_file_join ./text/join.txt
+#    read_file_join ./text/join.txt
     if [ $? -eq 1 ];then
         success_num=`expr ${success_num} + 1`
 #        echo 'success create meeting' ${success_num}
@@ -79,8 +88,9 @@ function do_work() {
 
 echo "" | tee ./text/1.txt ./text/2.txt ./text/3.txt ./text/4.txt ./text/5.txt ./text/create.txt ./text/join.txt \
 ./text/success.txt ./text/fail.txt
-start_time=`date`                  #定义脚本运行的开始时间
+start_time=`date`                  # 定义脚本运行的开始时间
 
+# todo main function
 for((i=0;i<n;i++))
 do
         sleep 1
@@ -88,9 +98,9 @@ do
 #        ./hello.sh| tee ./text/2.txt &&do_work ./text/2.txt
 #        ./create.sh 4 |tee ./text/1.txt |sleep 2 |exit && do_work ./text/1.txt
 done
-# todo main function
 
-stop_time=`date`                   #定义脚本运行的结束时间
+
+stop_time=`date`                   # 定义脚本运行的结束时间
 #count_result
 echo 'success_num:'${success_num}
 echo 'fail_num:'${fail_num}
--- demo.sh
+++ demo.sh
@@ -1,34 +1,52 @@
 #! /bin/bash
 
-starttime=`date`
-
-# todo main
-# for(i=0;i<set_num;i++);
-# ./create.sh $1 $2| tee 1.txt| ./killall.sh $1
-#./create.sh $1 $2| tee 1.txt| ./killall.sh $1
-#do_work
-function demo(){
-    for((i=0;i<5;i++))
+function read_file_create(){
+    i=0
+    for line in `cat 1.txt`
     do
-    {
-#        ./create.sh 4
-#        ./create.sh 5
-#        ./create.sh 6
-#        ./create.sh 7
-#        ./create.sh 8
-        ./hello.sh
-    } &
+        if [ `echo ${line} | grep -E "10[0-9]{6}"` ];then
+#        if [ `echo ${line} | grep -E "[0-9]{5}"` ];then
+            echo "meeting id" ${line} >>./text/create.txt
+#            echo "meeting id" ${line}
+            i=`expr ${i} + 1`
+
+#            ./join.sh ${ClientPerRoom} ${line} ${MeetingNum}
+#            return 1
+        fi
     done
-    wait
+    return 0
 }
-
-for((n=1;n<5;n++))
-do
-    sleep 1
-    demo
-done
-
-echo 'starttime' ${starttime}
-echo 'endtime' `date`
-
-exit 0
+read_file_create
+echo $i
+#starttime=`date`
+#
+## todo main
+## for(i=0;i<set_num;i++);
+## ./create.sh $1 $2| tee 1.txt| ./killall.sh $1
+##./create.sh $1 $2| tee 1.txt| ./killall.sh $1
+##do_work
+#function demo(){
+#    for((i=0;i<5;i++))
+#    do
+#    {
+##        ./create.sh 4
+##        ./create.sh 5
+##        ./create.sh 6
+##        ./create.sh 7
+##        ./create.sh 8
+#        ./hello.sh
+#    } &
+#    done
+#    wait
+#}
+#
+#for((n=1;n<5;n++))
+#do
+#    sleep 1
+#    demo
+#done
+#
+#echo 'starttime' ${starttime}
+#echo 'endtime' `date`
+#
+#exit 0
