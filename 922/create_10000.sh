1383f96b5310606ec0a1b9d96036b5487a7c1be5
wenlizhe <lizhe.wen@juphoon.com.cn>
2017-12-12 11:51:55 +0800

    feture：新增create_10000.sh注释

--- 922/create_10000.sh
+++ 922/create_10000.sh
@@ -33,15 +33,22 @@ function getTiming(){
 
 # 利用管道进行多线程控制，调用创建会议脚本
 function main(){
+    # 创建有名管道
     [ -e ./fd1 ] || mkfifo ./fd1
+    # 创建文件描述符，以可读（<）可写（>）的方式关联管道文件，这时候文件描述符3就有了有名管道文件的所有特性
     exec 3<> ./fd1
+    # 关联后的文件描述符拥有管道文件的所有特性,所以这时候管道文件可以删除，我们留下文件描述符来用就可以了
     rm -rf ./fd1
+    # 创建令牌
     for i in `seq 1 2`;
     do
+    # echo 每次输出一个换行符,也就是一个令牌
         echo >&3
     done
+    # 拿出令牌，进行并发操作
     for((i=0;i<num;i++))
     do
+        # read 命令每次读取一行，也就是拿到一个令牌
         read -u3
         {
             ./hello.sh
@@ -51,11 +58,14 @@ function main(){
 #            ./create.sh 7
 #            ./create.sh 8
 #            sleep 1
+            # 执行完一条命令会将令牌放回管道
             echo >&3
         }&
     done
     wait
+    # 关闭文件描述符的读
     exec 3<&-
+    # 关闭文件描述符的写
     exec 3>&-
 }
 
--- test.sh
+++ test.sh
@@ -1,2 +1,40 @@
 #!/bin/bash
 
+
+function print_line_num(){
+    l=`cat 1.txt |wc -l`
+    l=`expr ${l} + 1`
+    i=0
+    cat 1.txt | while read line
+    do
+        i=`expr ${i} + 1`
+        if [ ${i} == 10 ]
+        then
+           echo ${line}
+        elif [ ${i} -gt 10 ]
+        then
+           echo "a 大于 b"
+        elif [ ${i} -lt 10 ]
+        then
+           break
+        else
+           echo "没有符合的条件"
+        fi
+        echo ${line}
+    done
+#    while [ ${l} -ge 10 ]
+#    do
+#        pass
+#    done
+
+
+#    echo ${l}
+}
+
+
+function main(){
+    print_line_num
+}
+
+# echo ''>1.txt
+main
